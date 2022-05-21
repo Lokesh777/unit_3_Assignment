9e5c5495c7dec61c4d07e4ad84925879826c8f03
Lokesh777 <lokeshdevgan777@gmail.com>
2022-05-21 18:32:37 +0530

    now dice rolling properly

--- diceRoll/index.js
+++ diceRoll/index.js
@@ -1,13 +1,4 @@
 function playGame(){
-    var randNum1=Math.floor(Math.random()*6)+1;
-    var randImg1Path=randNum1;
-
-    var randNum2=Math.floor(Math.random()*6)+1;
-    var randImg2Path=randNum2;
-
-    var randNum3=Math.floor(Math.random()*6)+1;
-    var randImg3Path=randNum3;
-
     var img1=document.querySelectorAll("h1")[0];
     img1.setAttribute.randImg1Path;
     
@@ -17,49 +8,105 @@ function playGame(){
     var img3=document.querySelectorAll("h1")[2];
     img3.setAttribute.randImg3Path;  
 
-    if(randNum1){
+    
+    var randNum1=Math.floor(Math.random()*6)+1;
+    var randImg1Path=randNum1;
+
+    var randNum2=Math.floor(Math.random()*6)+1;
+    var randImg2Path=randNum2;
+
+    var randNum3=Math.floor(Math.random()*6)+1;
+    var randImg3Path=randNum3;
+
+    
+
+    // if(randNum1){
         if(randNum1>randNum2&&randNum1>randNum3){
             document.querySelector("#result").innerHTML="Mamber A is winner"
-            document.querySelector("h1").style.backgroundColor="green";
-         }
-          else{
-        document.querySelector("h1").style.backgroundColor="yellow";
-    }
-         
-    }
-    else{
-        document.querySelector("h1").style.backgroundColor="yellow";
-    }
-
-   if(randNum2){
-        if(randNum2>randNum3&&randNum2>randNum1){
-            document.querySelector("#result").innerHTML="Mamber B is winner"
-            document.querySelector("h1").style.backgroundColor="green";
+            // document.querySelector("h1").style.backgroundColor="green";
+                    if(randNum2>randNum3){
+                        img1.style.backgroundColor="green";
+                        img2.style.backgroundColor="yellow";
+                        img3.style.backgroundColor="red";
+                    }else{
+                        img1.style.backgroundColor="green";
+                        img2.style.backgroundColor="red";
+                        img3.style.backgroundColor="yellow";
+                    }
           }
-     }
+    // else{
+    //     document.querySelector("h1").style.backgroundColor="yellow";
+    // }
+
+//    if(randNum2){
+       else if(randNum2>randNum3&&randNum2>randNum1){
+            document.querySelector("#result").innerHTML="Mamber B is winner"
+            // document.querySelector("h1").style.backgroundColor="green";
+            if(randNum1>randNum2){
+                img1.style.backgroundColor="yellow";
+                img2.style.backgroundColor="green";
+                img3.style.backgroundColor="red";
+            }else{
+                img1.style.backgroundColor="red";
+                img2.style.backgroundColor="green";
+                img3.style.backgroundColor="yellow";
+            }
+        }
+    //  }
    
 
 
-   if(randNum3){
-            if(randNum3>randNum1&&randNum3>randNum2){
+//    if(randNum3){
+    else if(randNum3>randNum1&&randNum3>randNum2){
             document.querySelector("#result").innerHTML="Mamber C is winner"
-            document.querySelector("h1").style.backgroundColor="green";
+            // document.querySelector("h1").style.backgroundColor="green";
+            if(randNum1>randNum2){
+                img1.style.backgroundColor="yellow";
+                img2.style.backgroundColor="red";
+                img3.style.backgroundColor="green";
+            }else{
+                img1.style.backgroundColor="red";
+                img2.style.backgroundColor="yellow";
+                img3.style.backgroundColor="green";
+            }
         }
-   }
-   else{
-    document.querySelector("h1").style.backgroundColor="yellow";
-   }
+//    }
+//    else{
+//     document.querySelector("h1").style.backgroundColor="yellow";
+//    }
 
 
-    if(randNum1==randNum2||randNum1==randNum3){
-        document.querySelector("#result").innerHTML="Draw"
-    document.querySelector("h1").style.backgroundColor="yellow";
-
+    else if(randNum1==randNum2==randNum3){
+                img1.style.backgroundColor="blue";
+                img2.style.backgroundColor="blue";
+                img3.style.backgroundColor="blue";
+            
     }
     
-      
+    else if(randNum1==randNum2&&randNum1>randNum3&&randNum2>randNum3){
+        img1.style.backgroundColor="blue";
+        img2.style.backgroundColor="blue";
+        document.querySelector("#result").innerHTML="Mamber A&B is winner"
+         
+        // img3.style.backgroundColor="blue";
+    }
+    else if(randNum2==randNum3&&randNum2>randNum1&&randNum3>randNum1){
+        img3.style.backgroundColor="blue";
+        img2.style.backgroundColor="blue";
+        document.querySelector("#result").innerHTML="Mamber B&C is winner"
+         
+        // img3.style.backgroundColor="blue";
+    }
+    else if(randNum3==randNum1&&randNum1>randNum2&&randNum3>randNum2){
+        img1.style.backgroundColor="blue";
+        img3.style.backgroundColor="blue";
+        document.querySelector("#result").innerHTML="Mamber A&C is winner"
+         
+        // img3.style.backgroundColor="blue";
+    }
+}   
    
-}
+// }
 document.querySelector("button").addEventListener("click",playGame)
 
 
